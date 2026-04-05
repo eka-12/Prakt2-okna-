68e798c186e3f75fe64dc811880f597056f2fd70
eka-12 <ekare@sfedu.ru>
2026-04-05 22:11:44 +0300

    Update MainWindow.xaml.cs

--- Prakt2(okna)/MainWindow.xaml.cs
+++ Prakt2(okna)/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Prakt2_okna_;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Reflection;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,22 +14,96 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml.Linq;
 
 namespace Prakt2_okna_
 {
     /// <summary>
-    /// Логика взаимодействия для MainWindow.xaml
+    /// Interaction logic for MainWindow.xaml
     /// </summary>
     public partial class MainWindow : Window
+{
+    bool Choised = false;
+
+    public MainWindow()
     {
-        public MainWindow()
+        InitializeComponent();
+    }
+
+    private void bRegistration_Click(object sender, RoutedEventArgs e)
+    {
+        Choised = false;
+        tbLogin.IsEnabled = true;
+        tbPass1.IsEnabled = true;
+        tbPass2.IsEnabled = true;
+        bEnter.IsEnabled = true;
+    }
+
+    private void bLogin_Click(object sender, RoutedEventArgs e)
+    {
+        Choised = true;
+        tbLogin.IsEnabled = true;
+        tbPass1.IsEnabled = true;
+        tbPass2.IsEnabled = false;
+        bEnter.IsEnabled = true;
+    }
+
+    private void Registration(string tbL, string tbP1, string tbP2)
+    {
+        Window1 workWindow = new Window1();
+        if (tbL == "")
         {
-            InitializeComponent();
+            MessageBox.Show("Введите логин");
+            return;
         }
-
-        private void tbLogin_TextChanged(object sender, TextChangedEventArgs e)
+        if (tbP1 == tbP2)
         {
+            /*
+            проверить логин на незанятость если такого нет
+            зарегистрировать пользователя, иначе сообщить об ошибке
+            */
+        }
+        else
+        {
+            MessageBox.Show("Пароли различны");
+            return;
+        }
+        workWindow.lUser.Content = "Текущий пользователь: " + tbL;
+        workWindow.Show();
+        Close();
+    }
 
+    private void Logining(string tbL, string tbP1)
+    {
+        Window1 workWindow = new Window1();
+        if (tbL == "")
+        {
+            MessageBox.Show("Введите логин");
+            return;
+        }
+        /*
+        Добавить проверку логина и пароля из БД если они
+        верны пустить дальше, иначе сообщить об ошибке
+        */
+        workWindow.lUser.Content = "Текущий пользователь:" + tbL;
+        workWindow.Show();
+        Close();
+    }
+
+    private void bEnter_Click(object sender, RoutedEventArgs e)
+    {
+        if (Choised == false)
+        {
+            Registration(tbLogin.Text, tbPass1.Text, tbPass2.Text);
+        }
+        else
+        {
+            Logining(tbLogin.Text, tbPass1.Text);
+        }
+    }
+
+  }
+}
         }
     }
 }
